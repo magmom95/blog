b241ed9bc876c907ae4a3575cee560549d3ea0b8
magmom95 <magmom95@naver.com>
2023-11-21 15:33:48 +0900

    ing

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,21 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Google OTP Login',
+    description: '구글 OTP 로그인 설계 및 개발 과정',
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://forest-screw-40a.notion.site/GOTP-Google-OTP-50ef1c620b3049ce8d373d4e2e117e3b',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Agent Download',
+    description: `Redux에 Utils 에 Version 값을 useSelector 통해 Version Check를 설게 및 개발 과정 `,
+    imgSrc: '/static/images/agentDownload.png',
+    href: 'https://forest-screw-40a.notion.site/LOGIN-Agent-Downlaods-36d0fc77e0b940b7a60cda72ecab0dbe',
+  },
+  {
+    title: '보고서 생성',
+    description: `선택한 자산에 대한 보고서를 CVE ID 혹은 Product(취약점 발생 제품 식별 코드) 생성 되며 보고서 생성 단계는 보고서 유형, 선택 보고서, 생성 확인 단계를 설게 및 개발 과정`,
+    imgSrc: '/static/images/Report.png',
+    href: 'https://forest-screw-40a.notion.site/08659041be8d455eb7b03423212fc49a',
   },
 ]
 
